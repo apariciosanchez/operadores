d3ed728b69e28ff824540d323937917e6a32e4d4
Karla Cruz <kcruz080492@gmail.com>
2024-10-29 11:19:30 -0600

    Update arraymedia.js
    Hice algunos cambios, si los demás los tienes igual, haz el cambio para poder practicar el ciclo For

--- arraymedia.js
+++ arraymedia.js
@@ -1,11 +1,15 @@
 
 let numeros = [5, 9, 10, 12];
+//Se inicializa la variable para la suma 
+let suma =0
+// esto esta bien, pero la guia consistia en poner en practica el ciclo FOR
+//let suma = numeros.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
 
-
-let suma = numeros.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
-
+//Bucle para suma todos los elementos del array
+for (let i = 0; i < numeros.length; i++){
+  suma += numeros[i];
+}
 
 let media = suma / numeros.length;
-
 
 console.log(`La media es ${media}`);
